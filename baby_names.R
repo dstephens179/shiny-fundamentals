be23fad84aaea484f72847d1cf29d840c7a2fb3c
dstephens179 <david.stephens365@gmail.com>
2022-02-07 13:50:45 -0500

    Initial upload.

---
+++ baby_names.R
@@ -0,0 +1,28 @@
+library(shiny)
+library(tidyverse)
+
+ui <- fluidPage(
+  titlePanel("Baby Name Explorer"),
+  sidebarLayout(
+    sidebarPanel(
+      textInput('name', 'Enter name', 'David')
+    ),
+    mainPanel(
+      plotOutput('trend')
+    )
+  )
+)
+  
+server <- function(input, output, session){
+  output$trend <- renderPlot({
+    data_name <- subset(
+      babynames, name == input$name
+    )
+    ggplot(data_name) +
+      geom_line(
+        aes(x = year, y = prop, color = sex)
+      )
+  })
+}
+  
+shinyApp(ui = ui, server = server)
